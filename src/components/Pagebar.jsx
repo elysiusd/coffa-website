9bba156298dc1c7986b33d5ce07811e5e5089a2b
Dennis Destiny <143692938+elysiusd@users.noreply.github.com>
2025-01-07 02:26:27 +0000

    Update Pagebar.jsx

--- src/components/Pagebar.jsx
+++ src/components/Pagebar.jsx
@@ -3,7 +3,8 @@ import { IoMdMenu } from "react-icons/io";
 import { FaUser } from "react-icons/fa6";
 import { FaSearch } from "react-icons/fa";
 import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
+
 
 
 function Pagebar() {
@@ -50,18 +51,18 @@ function Pagebar() {
                     </div>
                 <div className='hidden lg:flex'>
                     <ul className='flex flex-row gap-10 uppercase'>
-                        <li className={`text-lg ${isActive(["/home", "/]"]) ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><a href='/home'>Home</a></li>
-                        <li className={`text-lg ${isActive("/about") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><a href='/about'>About</a></li>
-                        <li className={`text-lg ${isActive("/coffee") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><a href='/coffee'>Coffees</a></li>
+                        <li className={`text-lg ${isActive("/home") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><Link to='/home'>Home</Link></li>
+                        <li className={`text-lg ${isActive("/about") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><Link to='/about'>About</Link></li>
+                        <li className={`text-lg ${isActive("/coffee") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><Link to='/coffee'>Coffees</Link></li>
                         <li className="text-lg hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer">Blog</li>
-                        <li className={`text-lg ${isActive("/contact") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><a href='/contact'>Contact</a></li>
+                        <li className={`text-lg ${isActive("/contact") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><Link to='/contact'>Contact</Link></li>
                     </ul>
                 </div>
                 <div className='lg:block hidden '>
                     <ul className='flex flex-row gap-5 uppercase'>
                         <li className="text-lg hover:text-red-600  py-4 ease-in-out duration-1000 cursor-pointer"><FaUser size={20} /> </li>
                         <li className="text-lg hover:text-red-600  py-4 ease-in-out duration-1000 cursor-pointer">Login</li>
-                        <li className="text-lg hover:text-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer "><FaSearch size={20} />                        </li>
+                        <li className="text-lg hover:text-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer "><FaSearch size={20} />   </li>
                     </ul>
                 </div>
                 </div>
@@ -72,12 +73,12 @@ function Pagebar() {
   } left-96 lg:hidden`}>
             <div className='flex justify-center w-80%'>
                     <ul className='flex flex-col gap-3 uppercase'>
-                        <li className={`text-lg ${isActive(["/home","/"]) ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><a href='/home'>Home</a></li>
-                        <li className={`text-lg ${isActive("/about") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><a href='/about'>About</a></li>
-                        <li className={`text-lg ${isActive("/coffee") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><a href='/coffee'>Coffees</a></li>
+                         <li className={`text-lg ${isActive("/home") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><Link to='/home'>Home</Link></li>
+                        <li className={`text-lg ${isActive("/about") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><Link to='/about'>About</Link></li>
+                        <li className={`text-lg ${isActive("/coffee") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><Link to='/coffee'>Coffees</Link></li>
                         <li className="text-lg hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer">Blog</li>
-                        <li className={`text-lg ${isActive("/contact") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><a href='/contact'>Contact</a></li>
-                    </ul>
+                        <li className={`text-lg ${isActive("/contact") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><Link to='/contact'>Contact</Link></li>
+                   </ul>
                 </div>
                 <div className=' '>
                     <ul className='flex flex-row gap-5 uppercase'>
