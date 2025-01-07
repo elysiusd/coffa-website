a0ca1545dd4ba2883f04dc54d05da163eb562115
Dennis Destiny <143692938+elysiusd@users.noreply.github.com>
2025-01-07 02:21:50 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,16 @@
-import bannerBg from '../assets/images/banner-bg.png'
+import bannerBg from '../assets/images/header-bg.png'
 import { IoMdMenu } from "react-icons/io";
 import { FaUser } from "react-icons/fa6";
 import { FaSearch } from "react-icons/fa";
 import { useState } from 'react';
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import gsap from 'gsap';
-import { useRef, useEffect } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 
 
-function Navbar() {
+
+function Pagebar() {
     const location = useLocation();
 
     const isActive = (path) => location.pathname === path;
-    const divRef = useRef(null);
-
-    useEffect(() => {
-      // Trigger the slide-away animation when the component mounts
-      gsap.to(divRef.current, {
-        x: "-100%", // Moves the div 100% of its width to the right
-        duration: 4, // Animation duration in seconds
-        ease: "power4.out", // Smooth easing
-      });
-
-    }, []); // Empty dependency array ensures this runs once after mount
-
-    
-  
-   
 
     const [click, setClick] = useState(false);
 
@@ -53,30 +34,17 @@ function Navbar() {
             text: `more-or-less normal distribution of letters, as opposed to using`
         },
     ]
-    const settings = {
-        infinite: true,
-        speed: 700,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        nextArrow: <FaUser/>,
-        prevArrow: <FaUser/>
-    }
-
 
     return (
         <nav className='w-full'>
         
-            <div className=" text-white h-fit lg:h-[750px] bg-cover" style={{
+            <div className=" text-white h-fit lg:h-[150px] bg-cover" style={{
             backgroundImage: `url(${bannerBg})`,
         }}>
-            <div ref={divRef} className='bg-black h-[750px] w-full absolute box opacity-15'>
-            
-            </div>
             <div className='lg:w-[1240px] mx-auto px-10 py-10'>
                 <div className="flex flex-row justify-between">
                     <div>
-                       <h1 className="font-bold text-5xl font-sans uppercase">Coffa </h1>
+                       <h1 className="font-bold text-5xl font-sans uppercase"><a href='/home'>Coffa</a> </h1>
                     </div>
                     <div className='lg:hidden bg-white text-black px-4 rounded-md  hover:bg-red-600 ease-in-out duration-1000' onClick={handleNav}>
                         <IoMdMenu size={40} />
@@ -92,7 +60,7 @@ function Navbar() {
                 </div>
                 <div className='lg:block hidden '>
                     <ul className='flex flex-row gap-5 uppercase'>
-                        <li className="text-lg  hover:text-red-600  py-4 ease-in-out duration-1000 cursor-pointer"><FaUser size={20} /> </li>
+                        <li className="text-lg hover:text-red-600  py-4 ease-in-out duration-1000 cursor-pointer"><FaUser size={20} /> </li>
                         <li className="text-lg hover:text-red-600  py-4 ease-in-out duration-1000 cursor-pointer">Login</li>
                         <li className="text-lg hover:text-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer "><FaSearch size={20} />                        </li>
                     </ul>
@@ -105,44 +73,25 @@ function Navbar() {
   } left-96 lg:hidden`}>
             <div className='flex justify-center w-80%'>
                     <ul className='flex flex-col gap-3 uppercase'>
-                        <li className={`text-lg ${isActive("/home") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><Link to='/home'>Home</Link></li>
+                         <li className={`text-lg ${isActive("/home") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><Link to='/home'>Home</Link></li>
                         <li className={`text-lg ${isActive("/about") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><Link to='/about'>About</Link></li>
                         <li className={`text-lg ${isActive("/coffee") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><Link to='/coffee'>Coffees</Link></li>
                         <li className="text-lg hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer">Blog</li>
                         <li className={`text-lg ${isActive("/contact") ? 'bg-red-600 px-6 py-4 cursor-pointer':'hover:bg-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer'}`}><Link to='/contact'>Contact</Link></li>
-                    </ul>
+                   </ul>
                 </div>
                 <div className=' '>
                     <ul className='flex flex-row gap-5 uppercase'>
                         <li className="text-lg hover:text-red-600  py-4 ease-in-out duration-1000 cursor-pointer"><FaUser size={20} /> </li>
                         <li className="text-lg hover:text-red-600  py-4 ease-in-out duration-1000 cursor-pointer">Login</li>
-                        <li className="text-lg hover:text-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer "><FaSearch size={20} />                        </li>
+                        <li className="text-lg hover:text-red-600 px-6 py-4 ease-in-out duration-1000 cursor-pointer "><FaSearch size={20} /></li>
                     </ul>
                 </div>
             </div>
-
-                <div className=''>
-                <div className='my-20'>
-                <Slider {...settings}>
-                        {navSlideData.map((d) => (
-                            <div className=''>
-                            <h1 className='font-bold text-7xl font-mono pb-10'>{d.header} <br/> {d.header2}</h1>
-                            <p className='pb-10'>{d.text}</p>
-                            <div className='flex gap-2'>
-                            <button className='bg-red-600 px-6 py-4 uppercase rounded-md'>About Us</button>
-                            <button className='bg-white text-black px-6 py-4 uppercase rounded-md'>Call now</button>
-                            </div>
-                            </div>
-                        ))}
-                </Slider>
-                </div>
-        
-
-                </div>
             </div>
             </div>
         </nav>
     );
 }
 
-export default Navbar;
+export default Pagebar;
